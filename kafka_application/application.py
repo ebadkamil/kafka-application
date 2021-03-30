522b2a37e6f48c80bfd9d6e9069c6c372878bbba
Ebad Kamil <ebad.kamil@ess.eu>
2021-03-30 12:54:55 +0200

    Include logger in application

--- kafka_application/application.py
+++ kafka_application/application.py
@@ -5,9 +5,11 @@ All rights reserved.
 """
 
 import argparse
+import logging
 import subprocess
 
 from kafka_application.kafka_helpers.machine_load_reporter import LoadReporter
+from kafka_application.utilities.logger import get_logger
 
 
 def start_kafka_services():
@@ -28,14 +30,34 @@ def start_application():
         "--grafana-carbon-address",
         required=False,
         help="<host[:port]> Address to the Grafana (Carbon) metrics server",
-        type=str)
+        type=str,
+    )
+
+    _log_levels = {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL,
+    }
+    parser.add_argument(
+        "--log-level",
+        required=False,
+        help="",
+        choices=[level.upper() for level in _log_levels.keys()],
+        type=lambda s: s.upper(),
+        default="INFO",
+    )
 
     args = parser.parse_args()
+
+    logger = get_logger("Kafka-application", level=_log_levels[args.log_level])
 
     load_reporter = None
     if args.grafana_carbon_address:
         load_reporter = LoadReporter(
-            args.grafana_carbon_address, update_interval_s=1)
+            args.grafana_carbon_address, logger, update_interval_s=1
+        )
 
     try:
         app = Application()
@@ -44,11 +66,12 @@ def start_application():
             load_reporter_t = load_reporter.start()
 
     except KeyboardInterrupt:
-        print("Cancelled by user")
+        load_reporter.stop()
 
     finally:
         if load_reporter:
             load_reporter_t.join()
+
 
 if __name__ == "__main__":
     start_application()
--- kafka_application/kafka_helpers/machine_load_reporter.py
+++ kafka_application/kafka_helpers/machine_load_reporter.py
@@ -4,11 +4,12 @@ Author: Ebad Kamil <ebad.kamil@ess.eu>
 All rights reserved.
 """
 
+import logging
 import os
 import sys
 import time
-import graphyte  # type: ignore
 
+import graphyte  # type: ignore
 import numpy as np
 import psutil as ps
 
@@ -19,23 +20,29 @@ class LoadReporter:
     def __init__(
         self,
         graphyte_server: str,
+        logger: logging.Logger,
         prefix: str = "machine_info",
         update_interval_s: int = 10,
     ):
         self._graphyte_server = graphyte_server
+        self._logger = logger
         self._update_interval_s = update_interval_s
-
+        self._stop = False
         self._sender = graphyte.Sender(self._graphyte_server, prefix=prefix)
 
     @run_in_thread
     def start(self):
-        while True:
+        while not self._stop:
             timestamp = time.time()
             memory = (ps.virtual_memory()).used / 1024 ** 3
             load = ps.cpu_percent()
             try:
                 self._sender.send("cpu_load", load, timestamp)
                 self._sender.send("memory", memory, timestamp)
+                self._logger.debug(f"load {load}: memory {memory}")
             except Exception as ex:
-                print(f"Could not send load information: {ex}")
+                self._logger.error(f"Could not send load information: {ex}")
             time.sleep(self._update_interval_s)
+
+    def stop(self):
+        self._stop = True
