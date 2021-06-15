7c1158b6214850a954418aa85654f4e59c63e99d
Ebad Kamil <ebad.kamil@ess.eu>
2021-06-15 14:24:14 +0200

    Add a kafka producer wrapper

--- kafka_application/helpers/producer.py
+++ kafka_application/helpers/producer.py
@@ -1,59 +1,45 @@
-from confluent_kafka import Producer
+
+from threading import Thread
+import time
+
 from faker import Faker
-import numpy as np
+from confluent_kafka import Producer
 
 
-def delivery_report(err, msg):
-    """
-        err:
-        msg: has methods like partitions(), headers(), value() etc
-    """
-    if err:
-        print(f"Message delivery failed: {err}")
-    else:
-        print(f"Message delivered to partition {msg.partition()} - "
-              f"Offset : {msg.offset()} "
-              f"{msg.value()}")
+class KafkaProducer:
+    def __init__(self, producer):
+        self._producer = producer
+        self._cancelled = False
+        self._poll_thread = Thread(target=self._poll_loop)
+        self._poll_thread.start()
 
+    def _poll_loop(self):
+        while not self._cancelled:
+            self._producer.poll(0.5)
 
-def run_producer():
-    producer = Producer(
-        {'bootstrap.servers':'localhost:9092',
-        # 'security.protocol' : 'sasl_ssl',
-        # 'sasl.mechanism' : 'SCRAM-SHA-512',
-        # 'sasl.username' : 'some-user',
-        # 'sasl.password' : 'some-password'
-        # 'sasl.ca.location': '/home/ebadkamil/kafka...'
-        'acks': '-1', # writing to brokers, they acknowledge to producer (0, 1 , -1)
-        # 0 No ack, 1 producer will wait until leader replica is written,
-        # -1 wait until it receives ack from all replicas/broker,
-        'partitioner':'consistent_random'
-        }
-    )
-    # topic_info = producer.list_topics()
-    # print(topic_info.topics)
-    for i in range(10):
-        msg = {
-            'id': i,
-            'name': Faker('en_US').name()
-        }
-        msg_header = {
-            'source': b'Ebad'
-        }
+    def close(self):
+        self._cancelled = True
+        self._poll_thread.join()
+        max_wait_to_publish_producer_queue = 2
+        self._producer.flush(max_wait_to_publish_producer_queue)
 
-        producer.poll(timeout=0) # Poll the producer for events or callbacks
-        producer.produce(
-            topic='demo-topic',
-            value=str(msg),
-            # timestamp=current_time by default,
-            headers=msg_header,
-            on_delivery = delivery_report) # async. Message gets pushed to buffer and returns.
-
-    producer.flush(
-        # timeout in seconds
-        ) # Wait for all messages in buffer to be delivered. Covenience method
-    # that calls poll() until len() of buffer is zero. Return number of messages in
-
-
-if __name__ == '__main__':
-    run_producer()
+    def produce(
+        self,
+        topic,
+        payload,
+        timestamp,
+    ):
+        def ack(err, msg):
+            if err:
+                print(f"Message failed delivery: {err}")
+            else:
+                print(f"Message delivered to partition {msg.partition()} - "
+                      f"Offset : {msg.offset()} "
+                      f"{msg.value()}")
+        try:
+            self._producer.produce(
+                topic, payload, on_delivery=ack, timestamp=timestamp
+            )
+        except Exception as ex:
+            print(f"Kafka message could not be published: {ex}")
+        self._producer.poll(0)
