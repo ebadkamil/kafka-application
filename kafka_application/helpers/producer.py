482181d4d57555cdeda15d992f17a2b77d42653c
Ebad Kamil <ebad.kamil@ess.eu>
2021-06-15 14:24:51 +0200

    Add run_producer method to test

--- kafka_application/helpers/producer.py
+++ kafka_application/helpers/producer.py
@@ -43,3 +43,35 @@ class KafkaProducer:
         except Exception as ex:
             print(f"Kafka message could not be published: {ex}")
         self._producer.poll(0)
+
+
+def run_producer():
+    producer = Producer(
+        {'bootstrap.servers':'localhost:9092',
+        # 'security.protocol' : 'sasl_ssl',
+        # 'sasl.mechanism' : 'SCRAM-SHA-512',
+        # 'sasl.username' : 'some-user',
+        # 'sasl.password' : 'some-password'
+        # 'sasl.ca.location': '/home/ebadkamil/kafka...'
+        'acks': '-1', # writing to brokers, they acknowledge to producer (0, 1 , -1)
+        # 0 No ack, 1 producer will wait until leader replica is written,
+        # -1 wait until it receives ack from all replicas/broker,
+        'partitioner':'consistent_random'
+        }
+    )
+
+    kafka_producer = KafkaProducer(producer)
+    for i in range(10):
+        msg = {
+            'id': i,
+            'name': Faker('en_US').name()
+        }
+
+        kafka_producer.produce(
+            'demo',
+            str(msg),
+            time.time()) # async. Message gets pushed to buffer and returns.
+
+
+if __name__ == '__main__':
+    run_producer()
